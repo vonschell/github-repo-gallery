12133b8395c774453be90efd291200793378b42d
Vonschell <vonschell02@gmail.com>
2024-12-18 08:21:28 +0900

    Fetching and displaying user info

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ const gitProfileInfo = async function() {
 };
 
 gitProfileInfo();
+
+const displayProfileInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>
+  `;
+  overview.append(div);
+};
