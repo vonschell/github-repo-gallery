3f703e8866b2df2c74cb5843688051b7a4278724
Vonschell <vonschell02@gmail.com>
2024-12-18 08:07:53 +0900

    Fetching api json data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 //Global variables
 //division where your profile information will appear
 const overview = document.querySelector(".overview");
-const username = "Vonschell";
+const username = "vonschell";
+
+const gitProfileInfo = async function() {
+    const profileInfo = await fetch (`https://api.github.com/users/${username}`);
+    const data = await profileInfo.json();
+    console.log(data);
+};
+
+gitProfileInfo();
