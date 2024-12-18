1333e145b393191831d27d25c06dc37630c7ad3d
Vonschell <vonschell02@gmail.com>
2024-12-18 09:53:59 +0900

    Created a function to get specific repo info

--- js/script.js
+++ js/script.js
@@ -52,6 +52,12 @@ const displayRepos = function (repos) {
 repoList.addEventListener("click", function (e) {
   if (e.target.matches("h3")) {
     const repoName = e.target.innerText;
-    console.log(repoName);
+    getRepoInfo(repoName);
   }
 });
+
+const getRepoInfo = async function (repoName) {
+  const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+  const repoInfo = await fetchInfo.json();
+  console.log(repoInfo);
+}
